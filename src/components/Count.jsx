fedb98d54e9511ed4b23dbe786e1c2e24c1fca57
peroni92 <marcoshperoni@hotmail.com>
2023-04-07 15:27:13 -0300

    refact count component

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -3,15 +3,18 @@ import { useState } from 'react'
 
 
 export default function Count() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    
+    const addCount = () => setCount((count) => count + 1)
+    const resetCount = () => setCount(0)
 
-  if (count == 9) {
-    setCount(0)
-  }
+    return (
+        <>
+            <button onClick={addCount}>
+                count is {count}
+            </button>
 
-  return (
-    <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-    </button>
-  )
+            <button onClick={resetCount}>Reset</button>
+        </>
+    )
 }
